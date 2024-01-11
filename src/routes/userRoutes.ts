b6396f38d9fdd5eb68cfabc223e7a9c841d9b84d
davit <dtnakhutsrishvili@gmail.com>
2024-01-11 13:49:32 +0400

    added user api

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -5,27 +5,55 @@ import User from '../models/userModel';
 export const userRoutes = Router();
 
 userRoutes.post('/register',async (req:Request, res:Response) => {
-    if(req.body){
-        
+    try {
+        var new_user = new User({ 
+            name:req.body.name,
+            email:req.body.email,
+            password:req.body.password,
+            phoneNumber:req.body.phoneNumber
+        }) 
+        await new_user.save()
+        res.status(201).send(new_user)
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
     }
-    var new_user = new User({ 
-        name:req.body.name,
-        email:req.body.email,
-        password:req.body.password,
-        phoneNumber:req.body.phoneNumber
-    }) 
-
-    await new_user.save()
-
-    res.send(new_user)
 });
 userRoutes.get('/getUsers',async(req:Request,res:Response)=>{
-    const users = await User.find()
-    res.send(users);
+    try{
+        const users = await User.find()
+        res.status(201).send(users)
+    }catch(error){
+        res.status(500).send('Internal Server Error');
+    }
+
 })
-userRoutes.patch('/updateUser',(req:Request,res:Response)=>{
-    res.send("What's up doc ?!");
+userRoutes.get('/getUser/:id',async(req:Request,res:Response)=>{
+    try{
+    const user=await User.findById(req.params.id)
+    res.status(201).send(user)
+    }catch(error){
+        res.status(500).send('Internal Server Error');
+    }
 })
-userRoutes.delete('/deleteUser',(req:Request,res:Response)=>{
-    res.send("What's up doc ?!");
+userRoutes.patch('/updateUser/:id',async(req:Request,res:Response)=>{
+    try{
+    await User.findByIdAndUpdate(req.params.id,{    
+            name:req.body.name,
+            email:req.body.email,
+            password:req.body.password,
+            phoneNumber:req.body.phoneNumber})
+
+        res.status(201).send("user updated")
+        }catch(error){
+            res.status(500).send('Internal Server Error');
+        }
+})
+userRoutes.delete('/deleteUser/:id',async(req:Request,res:Response)=>{
+    try{
+        console.log(req.params.id)
+        const user=await User.findByIdAndDelete(req.params.id)
+        res.status(201).send("user deleted")
+        }catch(error){
+            res.status(500).send('Internal Server Error');
+        }
 })
